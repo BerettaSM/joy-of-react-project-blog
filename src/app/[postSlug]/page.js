d0e37e01fbad9b620a218e404db27493ff8e81b0
BerettaSM <ramonsaviato@hotmail.com>
2024-01-02 12:59:25 -0300

    feat: exercise 3 done

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,16 +1,26 @@
 import React from 'react';
-
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { parseISO } from 'date-fns';
 
 import { loadBlogPost } from '@/helpers/file-helpers';
+import { BLOG_TITLE } from '@/constants';
 
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
 
-async function BlogPost({ params: { postSlug  } }) {
+export async function generateMetadata({ params: { postSlug } }) {
+    const {
+        frontmatter: { title, abstract },
+    } = await loadBlogPost(postSlug);
 
+    return {
+        title: `${title} • ${BLOG_TITLE}`,
+        description: abstract,
+    };
+}
+
+async function BlogPost({ params: { postSlug } }) {
     const { frontmatter, content } = await loadBlogPost(postSlug);
     const { title, publishedOn } = frontmatter;
 
@@ -18,9 +28,7 @@ async function BlogPost({ params: { postSlug  } }) {
         <article className={styles.wrapper}>
             <BlogHero title={title} publishedOn={parseISO(publishedOn)} />
             <div className={styles.page}>
-                <MDXRemote
-                    source={content}
-                />
+                <MDXRemote source={content} />
             </div>
         </article>
     );
